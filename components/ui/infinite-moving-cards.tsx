bfe1d7edd0c9c2a7df505c74c8cdbda5ad6502a3
Arthath Agary <arthathagary@gmail.com>
2024-04-21 16:03:12 +0530

    change image slider

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+
+import edu from "@/public/assets/images/edukinniya.png";
 
 export const InfiniteMovingCards = ({
   items,
@@ -77,24 +80,27 @@ export const InfiniteMovingCards = ({
         className
       )}
     >
+      <h1 className="text-white flex flex-col justify-center items-center font-bold text-4xl mb-2">
+        Trusted By
+      </h1>
       <ul
         ref={scrollerRef}
         className={cn(
           " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
-          start && "animate-scroll ",
+          start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
-            style={{
-              background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
-            }}
+            className=" max-w-full relative  flex-shrink-0  px-8 py-4 md:w-[250px]"
+            // style={{
+            //   background:
+            //     "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+            // }}
             key={item.name}
           >
-            <blockquote>
+            {/* <blockquote>
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
@@ -112,7 +118,8 @@ export const InfiniteMovingCards = ({
                   </span>
                 </span>
               </div>
-            </blockquote>
+            </blockquote> */}
+            <Image src={edu} alt="" height={100} />
           </li>
         ))}
       </ul>
